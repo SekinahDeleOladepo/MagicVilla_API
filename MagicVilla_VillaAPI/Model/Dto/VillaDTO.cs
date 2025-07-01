39ac02b31a52a62a2ff71c44a3f94f52957ed24a
sekinah <sekinahdeleoladepo@gmail.com>
2025-07-01 09:06:30 +0100

    Adjusted my Properties

--- MagicVilla_VillaAPI/Model/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Model/Dto/VillaDTO.cs
@@ -4,7 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Rate { get; set; }
+        public string Details { get; set; }
+        public Double Rate { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
         public string ImageUrl { get; set; }
--- MagicVilla_VillaAPI/Model/Villa.cs
+++ MagicVilla_VillaAPI/Model/Villa.cs
@@ -5,11 +5,12 @@
         public int Id { get; set; }
         public string Name{ get; set; }
         public string Details{ get; set; }
-        public int Rate { get; set; }
+        public double Rate { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
         public string ImageUrl { get; set; }
-        public int Amenity { get; set; }
+        public string Amenity { get; set; }
+        public DateTime UpdatedDate { get; set; }
         public DateTime DateCreated { get; set; }
     }
 }
